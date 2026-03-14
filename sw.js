896b58974c35f7953e135fc9ac425f94250e1578
ALEJANDRO SOTO BARQUERO <oasotob@yahoo.com>
2026-03-14 07:49:23 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'abc-rice-v1';
+const CACHE_NAME = 'abc-rice-v2';
 const ASSETS = [
     '/',
     '/index.html',
@@ -8,8 +8,23 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', event => {
+    self.skipWaiting(); // Forzar actualización inmediata
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim()) // Tomar control inmediatamente
     );
 });
 
